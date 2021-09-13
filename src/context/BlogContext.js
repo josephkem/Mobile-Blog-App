4ddd151b4d36bbc3596dc7eb9ac67ce3cbc2ee0e
josephkem <josephkemoy@gmail.com>
2021-09-12 20:33:57 -0400

    Added refactoring for context creation

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,28 +1,23 @@
 import React, { useReducer } from "react";
-
-const BlogContext = React.createContext();
+import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "add_blogpost":
-      return [...state, { title: `BLog Post #${state.length + 1}` }];
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
     default:
       return state;
   }
 };
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-  const addBlogPost = () => {
+const addBlogPost = (dispatch) => {
+  return () => {
     dispatch({ type: "add_blogpost" });
   };
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  );
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
