abede12d14a3bdd0093fbd945de795e44f9b6fd6
josephkem <josephkemoy@gmail.com>
2021-09-21 18:53:53 -0400

    Added function to trash icon to delete blog posts

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,17 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import { Context } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context);
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
   return (
     <View>
       <Button title="Add Post" onPress={addBlogPost} />
@@ -14,8 +21,12 @@ const IndexScreen = () => {
         renderItem={({ item }) => {
           return (
             <View style={styles.row}>
-              <Text style={styles.title}>{item.title}</Text>
-              <Feather style={styles.icon} name="trash-2" />
+              <Text style={styles.title}>
+                {item.title} - {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Feather style={styles.icon} name="trash-2" />
+              </TouchableOpacity>
             </View>
           );
         }}
@@ -38,6 +49,7 @@ const styles = StyleSheet.create({
   },
   icon: {
     fontSize: 24,
+    color: "red",
   },
 });
 
