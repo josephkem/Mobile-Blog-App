c4cf0d78eb608e1584b7334cd258139b189aa790
josephkem <josephkemoy@gmail.com>
2021-09-21 20:37:07 -0400

    Added components along with some styling to render a form the user to create a post

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,26 +1,51 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../context/BlogContext";
 import {
   View,
   Text,
   StyleSheet,
+  TextInput,
   FlatList,
   Button,
   TouchableOpacity,
 } from "react-native";
 
 function CreateScreen({ navigation }) {
-  const { state } = useContext(Context);
-  const blogPost = state.find(
-    (blogPost) => blogPost.id === navigation.getParam("id")
-  );
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   return (
     <View>
-      <Text>Create</Text>
+      <Text style={styles.label}>Title</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChange={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChange={(text) => setContent(text)}
+      />
+      <Button title="Add Blog Post" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5,
+  },
+});
 
 export default CreateScreen;
